de260cc65fb3ba00b6f92adbea578812dac305d9
takuya <takanotume24@gmail.com>
2015-05-03 04:41:01 +0900

    first

---
+++ gitset.sh
@@ -0,0 +1,6 @@
+echo "# arduino" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/takanotume24/arduino.git
+git push -u origin master
